fddcc5c127d109fb858f9643ab1a39cf22edb985
jakiyonep <akiyone0820@gmail.com>
2020-05-19 07:10:33 +0900

    first

--- nihongo/settings/production.py
+++ nihongo/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 import dj_database_url
 
