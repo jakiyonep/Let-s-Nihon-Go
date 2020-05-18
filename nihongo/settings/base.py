659f5f93aa0e26414b4d0c3ed1fee9775ab5ef5e
jakiyonep <akiyone0820@gmail.com>
2020-05-19 07:30:32 +0900

    aa

--- nihongo/settings/base.py
+++ nihongo/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = [True]
 
 
 # Application definition
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
 MEDIA_URL = '/media/'
 
@@ -132,3 +136,6 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
