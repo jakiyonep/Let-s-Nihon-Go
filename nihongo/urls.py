e0a0537e972de017daaad811962b4bbc1ab78939
jakiyonep <akiyone0820@gmail.com>
2020-05-19 11:34:34 +0900

    '

--- nihongo/urls.py
+++ nihongo/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
