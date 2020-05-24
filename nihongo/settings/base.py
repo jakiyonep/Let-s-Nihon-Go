b82f735b74ca8f909af02c02b8361ea108df4666
jakiyonep <akiyone0820@gmail.com>
2020-05-24 21:31:24 +0900

    a

--- nihongo/settings/base.py
+++ nihongo/settings/base.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'letsnihongobaru'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
