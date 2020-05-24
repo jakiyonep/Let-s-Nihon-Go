49a396a6804aa1b264f1a233b2f90f069b901f4e
jakiyonep <akiyone0820@gmail.com>
2020-05-24 20:54:27 +0900

    tail

--- nihongo/settings/base.py
+++ nihongo/settings/base.py
@@ -131,8 +131,6 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'letsnihongobaru'
