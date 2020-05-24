d22800e415e458571a3f1d1e3ac91ce0d137c882
jakiyonep <akiyone0820@gmail.com>
2020-05-24 16:13:05 +0900

    '

--- nihongo/settings/base.py
+++ nihongo/settings/base.py
@@ -150,8 +150,8 @@ if DEBUG:
     STATIC_URL = '/static/'
 else:
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # メディアファイルの設定。今回は「project」というプロジェクト名の例
